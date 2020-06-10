e64d4c961c6bacf6a5a278d300da416b7dd696b1
toppev <44984598+toppev@users.noreply.github.com>
2020-06-10 20:40:56 +0300

    Fix cookies expiring at the end of the session

--- src/config/sessionConfig.js
+++ src/config/sessionConfig.js
@@ -2,5 +2,6 @@ const cookieSession = require('cookie-session');
 
 module.exports = cookieSession({
     name: 'session',
+    expires: new Date(Date.now() + 315569520000), // 10 years lol
     secret: process.env.JWT_SECRET,
 });
