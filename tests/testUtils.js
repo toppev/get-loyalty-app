59015fa7d641ea494e8016dec26473912f87230c
toppev <44984598+toppev@users.noreply.github.com>
2020-06-12 15:43:58 +0300

    Recreate package-lock.json, disable indexing db in tests

--- tests/testUtils.js
+++ tests/testUtils.js
@@ -8,6 +8,7 @@ module.exports = {
 async function initDatabase(identifier) {
     const url = `mongodb://127.0.0.1/loyalty-${identifier}-test`;
     await mongoose.connect(url, {
+        autoIndex: false,
         useCreateIndex: true,
         useNewUrlParser: true,
         useUnifiedTopology: true,
