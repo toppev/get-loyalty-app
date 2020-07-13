6f5ad7d92e8fa474365ab21e82896b7285125438
toppev <44984598+toppev@users.noreply.github.com>
2020-07-14 00:00:48 +0300

    Fix tests (and captcha lol)

--- src/middlewares/captcha.js
+++ src/middlewares/captcha.js
@@ -1,20 +1,21 @@
 const request = require("request");
 const StatusError = require("../helpers/statusError");
 
-function verifyCAPTCHA() {
-    return (req, res, next) => {
-        const secret = process.env.CAPTCHA_SECRET_KEY;
-        const token = req.body.token;
-        const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${token}`;
-
-        request.post({ url }, function (error, response, body) {
-            if (body.success) {
-                next()
-            } else {
-                throw new StatusError('Invalid CAPTCHA token', 400)
-            }
-        })
+function verifyCAPTCHA(req, res, next) {
+    if (process.env.NODE_ENV === 'test') {
+        return next()
     }
+    const secret = process.env.CAPTCHA_SECRET_KEY;
+    const token = req.body.token;
+    const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${token}`;
+
+    request.post({ url }, function (error, response, body) {
+        if (body.success) {
+            next()
+        } else {
+            throw new StatusError('Invalid CAPTCHA token', 400)
+        }
+    })
 
 }
 
