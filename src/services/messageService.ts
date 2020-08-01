09c9337cbf6b506ad28c9aee8134490a68a5a3e7
toppev <44984598+toppev@users.noreply.github.com>
2020-08-01 14:47:14 +0300

    Fix resub on 200-299, add status code in the error message

--- src/services/messageService.ts
+++ src/services/messageService.ts
@@ -29,11 +29,11 @@ function useSubscribe(identifiers: string[]) {
             subscribedTo.push(id)
             subscribeMessage(user.id, id).then(res => {
                 const { status } = res;
-                // Resubscribe on timeout
-                if (status === 408 || status === 504) {
+                // Resubscribe on success or timeout
+                if ((status >= 200 && status < 300) || status === 408 || status === 504) {
                     resub(id)
                 } else {
-                    console.log('An error occurred while subscribing to messages. Not trying anymore.')
+                    console.log(`An error occurred while subscribing to messages. Not trying anymore. Status: ${status}`)
                 }
                 res.json().then(data => {
                     if (data.message) {
