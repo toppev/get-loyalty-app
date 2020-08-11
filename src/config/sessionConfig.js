651d60532f660431b4d3de119bb4f1daa0ea1515
toppev <44984598+toppev@users.noreply.github.com>
2020-08-12 01:44:04 +0300

    Session domain

--- src/config/sessionConfig.js
+++ src/config/sessionConfig.js
@@ -4,4 +4,5 @@ module.exports = cookieSession({
     name: 'session',
     expires: new Date(Date.now() + 315569520000), // 10 years lol
     secret: process.env.JWT_SECRET,
+    domain: 'getloyalty.app'
 });
