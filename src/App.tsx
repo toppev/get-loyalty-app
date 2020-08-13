d43b42fa84ad624e273da2fa2fdbb494ecdfa0a3
toppev <44984598+toppev@users.noreply.github.com>
2020-08-13 15:27:07 +0300

    Register if /user/profile returns 401

--- src/App.tsx
+++ src/App.tsx
@@ -29,12 +29,12 @@ function App() {
             .catch(err => {
                 // TODO: Option to login on other responses?
                 const status = err?.response?.status;
-                if (status === 403 || status === 404) {
+                if (status === 401 || status === 403 || status === 404) {
                     registerRequest()
                         .then(onLogin)
                         .catch(_err => setError('Could not register a new account. Something went wrong :('))
                 } else {
-                    window.alert(`Something went wrong :(\nError: ${err?.response.body || err.toString()}`)
+                    setError(`Something went wrong :(\nError: ${err?.response.body || err.toString()}`)
                 }
             })
         // eslint-disable-next-line react-hooks/exhaustive-deps
