38bf9a2f4b64576043e23b5b22684b0e2a52d620
toppev <44984598+toppev@users.noreply.github.com>
2020-08-19 12:10:36 +0300

    Rename session cookie
    Avoid overwriting the session cookie

--- src/config/sessionConfig.js
+++ src/config/sessionConfig.js
@@ -1,7 +1,7 @@
 const cookieSession = require('cookie-session');
 
 module.exports = cookieSession({
-    name: 'session',
+    name: 'loyalty_session',
     expires: new Date(Date.now() + 315569520000), // 10 years lol
     secret: process.env.JWT_SECRET,
     domain: process.env.COOKIE_DOMAIN
