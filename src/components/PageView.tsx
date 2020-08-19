23227490b028b9c526805228877cb88767cdcb30
toppev <44984598+toppev@users.noreply.github.com>
2020-08-19 15:10:46 +0300

    Do QR codes on html change

--- src/components/PageView.tsx
+++ src/components/PageView.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Page, { LOADING_HTML } from "../model/Page";
+import { replaceQRCodes } from "../util/QRCode";
 
 interface PageViewProps {
     page: Page
@@ -9,6 +10,8 @@ export default function (props: PageViewProps) {
 
     const { page } = props
     const html = page.html || LOADING_HTML
+
+    useEffect(replaceQRCodes, [html]);
 
     return (
         <div className="page-view" dangerouslySetInnerHTML={{ __html: html }}/>
--- src/util/QRCode.ts
+++ src/util/QRCode.ts
@@ -1,12 +1,15 @@
 import qrcode from "qrcode-generator"
 
-const DATA_IDENTIFIER: string = '[data-qrcode]'
+const QR_DATA_IDENTIFIER: string = '[data-qrcode]'
 
+/**
+ * Replaces elements identified with {@link QR_DATA_IDENTIFIER} with the actual QR code images.
+ */
 export function replaceQRCodes() {
 
-    const elements = document.querySelectorAll(DATA_IDENTIFIER)
+    const elements = document.querySelectorAll(QR_DATA_IDENTIFIER)
     elements.forEach(el => {
-        const code = el.attributes.getNamedItem(DATA_IDENTIFIER)?.value
+        const code = el.attributes.getNamedItem(QR_DATA_IDENTIFIER)?.value
         if (code) {
             const qr = qrcode(0, 'L')
             qr.addData(code)
