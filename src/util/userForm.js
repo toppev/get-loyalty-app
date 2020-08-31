4bc62f69b650aa170942da0c63c9d9082a1f8a1d
toppev <44984598+toppev@users.noreply.github.com>
2020-08-31 17:01:25 +0300

    Disable birthday field if valid birthday was set, prevents users from changing their birthday

--- src/util/userForm.js
+++ src/util/userForm.js
@@ -8,27 +8,30 @@ const getBirthdayField = () => document.getElementById("birthday-selector");
 const getSubmitBtn = () => document.querySelector(`#user-form-submit`)
 
 export function useUserFormInitialValues() {
-    const { user } = useContext(AppContext);
+    const { user } = useContext(AppContext)
     if (!user) return
 
     // Handle default values
 
     const ef = getEmailField()
-    if (ef) ef.value = user.email || "";
+    if (ef) ef.value = user.email || ""
 
     if (user.birthday) {
         const bd = new Date(Date.parse(user.birthday)).toISOString()
         const bf = getBirthdayField()
-        if (bf) bf.value = bd.slice(0, 10);
+        // e.g 1999-02-16
+        if (bf) bf.value = bd.slice(0, 10)
+        // Disable birthday field to prevent abuse
+        bf.disabled = true
     }
 
     // Handle form submission
     const form = getUserForm()
     if (form) {
         form.onsubmit = e => {
-            e.preventDefault();
+            e.preventDefault()
             if (form?.reportValidity() !== false) {
-                submitChanges();
+                submitChanges()
             }
         }
     }
