2f63c3885b2c00c833e6051207388466fda2cb3a
toppev <44984598+toppev@users.noreply.github.com>
2020-09-18 22:55:18 +0300

    Add very basic test

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import App from '../App';
+import { render } from 'react-dom';
+
+describe('(Component) App', () => {
+
+    it('renders without exploding', () => {
+        const div = document.createElement('div')
+        render(<App/>, div)
+    })
+
+})
