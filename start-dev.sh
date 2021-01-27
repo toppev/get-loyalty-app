676221cc2714ea1c8d7545ad4b301d0b3985b4ef
toppev <44984598+toppev@users.noreply.github.com>
2021-01-27 20:54:18 +0200

    Dev setup stuff

--- start-dev.sh
+++ start-dev.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
 echo "Starting development environment. This may take a few minutes..."
-echo "Using dev.docker-compose.yml"
 
+echo "The web apps are available at:"
+echo "http://localhost:3000"
+echo "http://localhost:3002"
+
+: '
+echo "Opening the web apps..."
+{
+  xdg-open 'http://localhost:3000' && xdg-open 'http://localhost:3002'
+} || echo "Failed to open browser"
+'
+
+echo "Using dev.docker-compose.yml"
 sudo docker-compose -f dev.docker-compose.yml -p easy-loyalty-app up --build
