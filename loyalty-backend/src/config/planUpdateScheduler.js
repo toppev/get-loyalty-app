9ed7d1c378c863f987921e9c924bd21a68e341cf
toppev <44984598+toppev@users.noreply.github.com>
2021-03-21 12:25:12 +0200

    Dont run plan update when testing

--- loyalty-backend/src/config/planUpdateScheduler.js
+++ loyalty-backend/src/config/planUpdateScheduler.js
@@ -1,10 +1,12 @@
 const planService = require('../services/planService');
 
-
-planService.updateUserPlan()
-    .then(() => console.log("User plan checked/updated for the first time"))
-
-setInterval(() => {
+if (process.env.NODE_ENV !== "test") {
     planService.updateUserPlan()
-        .then(() => console.log("User plan checked/updated"))
-}, 2 * 60 * 60 * 1000) // 2 hours
+        .then(() => console.log("User plan checked/updated for the first time"))
+
+    setInterval(() => {
+        planService.updateUserPlan()
+            .then(() => console.log("User plan checked/updated"))
+    }, 2 * 60 * 60 * 1000) // 2 hours
+
+}
