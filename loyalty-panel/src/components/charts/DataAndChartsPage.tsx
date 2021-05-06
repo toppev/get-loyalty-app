a4c7c3c8078ccdf0eb0eab51dc55e4f4464c8adc
toppev <44984598+toppev@users.noreply.github.com>
2021-05-06 15:50:03 +0300

    More charts, refactoring and stuff

--- loyalty-panel/src/components/charts/DataAndChartsPage.tsx
+++ loyalty-panel/src/components/charts/DataAndChartsPage.tsx
@@ -1,5 +1,5 @@
-import React from "react"
-import { Box, createStyles, LinearProgress, makeStyles, Theme } from "@material-ui/core"
+import React, { Fragment } from "react"
+import { Box, createStyles, Divider, LinearProgress, makeStyles, Theme, Typography } from "@material-ui/core"
 import { Line } from 'react-chartjs-2'
 import useRequest from "../../hooks/useRequest"
 import { listCustomers } from "../../services/customerService"
@@ -10,6 +10,10 @@ import useResponseState from "../../hooks/useResponseState"
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
+    title: {
+      fontSize: '38px',
+      color: 'ghostwhite',
+    },
     p: {
       fontSize: '14px',
       color: theme.palette.grey[600]
@@ -22,8 +26,8 @@ const useStyles = makeStyles((theme: Theme) =>
       margin: 'auto',
       minWidth: '95%',
       [theme.breakpoints.up('sm')]: {
-        minWidth: '500px',
-        margin: '25px',
+        minWidth: '600px',
+        margin: '40px',
       },
     },
     chartDates: {
@@ -32,6 +36,10 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     center: {
       textAlign: 'center'
+    },
+    divider: {
+      margin: '60px 0',
+      backgroundColor: theme.palette.grey[800]
     }
   }))
 
@@ -49,39 +57,74 @@ export default function () {
 
   const customerCreatedDates = customers.map(it => it.createdAt)
   const customerLastActiveDates = customers.filter(it => it.lastVisit).map(it => it.lastVisit!!)
+  const customerUsageLength = customers.filter(it => it.lastVisit).map(it => {
+    const lastVisit = it.lastVisit?.getTime() || it.createdAt.getTime()
+    return Math.max(0, lastVisit - it.createdAt.getTime())
+  })
 
   const today = new Date()
   const startOfYear = new Date()
-  startOfYear.setFullYear(2020, 0, 1)
+  startOfYear.setFullYear(today.getFullYear(), 0, 1)
+
+  const charts = [
+    {
+      id: 'new_customers',
+      component: (
+        <DateChart
+          title="New Customers"
+          label="new customers joined"
+          customDateString={"(" + startOfYear.getFullYear() + ")"}
+          dateData={customerCreatedDates}
+          fromDate={startOfYear}
+          toDate={today}
+        />
+      )
+    }, {
+      id: 'last_activity',
+      component: (
+        <DateChart
+          title="Last Activity"
+          label="customers last visit on the app"
+          customDateString={"(" + startOfYear.getFullYear() + ")"}
+          dateData={customerLastActiveDates}
+          fromDate={startOfYear}
+          toDate={today}
+        />
+      )
+    }, {
+      id: 'usage_length',
+      component: (
+        <UsageLengthChart
+          title="Usage Retention"
+          label="how many customers have used the app this long"
+          joinLastDiff={customerUsageLength}
+        />
+      )
+    }
+  ]
+
 
   return error ? (
     <RetryButton error={error}/>
   ) : (
     <div>
-      {loading && <LinearProgress/>}
       <p className={classes.p}>
         (work in progress)
       </p>
-      <Box display="flex" flexWrap="wrap">
-        <div className={classes.chart}>
-          <DateChart
-            title="New Customers"
-            label="new customers joined"
-            customDateString={"(" + startOfYear.getFullYear() + ")"}
-            dateData={customerCreatedDates}
-            fromDate={startOfYear}
-            toDate={today}
-          />
-          <DateChart
-            title="Last Activity"
-            label="customers last visit on the app"
-            customDateString={"(" + startOfYear.getFullYear() + ")"}
-            dateData={customerLastActiveDates}
-            fromDate={startOfYear}
-            toDate={today}
-          />
+      <div>
+        <Typography variant="h1" className={classes.title}>A few cool charts</Typography>
+        {loading && <LinearProgress/>}
+        <Box display="flex" flexWrap="wrap">
+          {charts.map(it => <Fragment key={it.id}>{it.component}</Fragment>)}
+        </Box>
+      </div>
+      <Divider className={classes.divider}/>
+      <div>
+        <Typography variant="h1" className={classes.title}>Export data & API</Typography>
+        <div style={{ height: '800px' }}>
+          <p>Coming soon...</p>
         </div>
-      </Box>
+      </div>
     </div>
   )
 }
@@ -91,8 +134,9 @@ const COMMON_LINE_CHART_OPTIONS = {
   scales: {
     yAxes: [{
       ticks: { beginAtZero: true, },
-    }]
-  }
+    }],
+
+  },
 }
 
 interface ChartProps {
@@ -133,12 +177,67 @@ function DateChart(props: DateChartProps) {
   }
 
   return (
-    <div>
+    <div className={classes.chart}>
       <div className={classes.center}>
         <h2 className={classes.chartTitle}>{props.title}</h2>
         <p className={classes.chartDates}>
           {customDateString || fromDate.toLocaleDateString() + "-" + toDate.toLocaleDateString()}
         </p>
+      </div>
+      {/* @ts-ignore  */}
+      <Line data={data} options={COMMON_LINE_CHART_OPTIONS}/>
+    </div>
+  )
+}
+
+interface RetentionChartProps extends ChartProps {
+  joinLastDiff: number[]
+}
+
+
+function UsageLengthChart(props: RetentionChartProps) {
+
+  const classes = useStyles()
+
+  const dayInMS = 1000 * 60 * 60 * 24
+  const usageLength = {
+    '> year': dayInMS * 30 * 12,
+    '> 6 months': dayInMS * 30 * 6,
+    '> 3 months': dayInMS * 30 * 3,
+    '> month': dayInMS * 30,
+    '> 14 days': dayInMS * 14,
+    '> week': dayInMS * 7,
+    '>= day': dayInMS,
+    'once': -1,
+  }
+  const usageLengthGroup = new Array(Object.keys(usageLength).length).fill(0)
+
+  props.joinLastDiff.forEach(diff => {
+    Object.values(usageLength).some((val, index) => {
+      if (diff > val) {
+        usageLengthGroup[index]++
+        return true
+      }
+      return false
+    })
+  })
+
+  const data = {
+    labels: Object.keys(usageLength),
+    datasets: [{
+      label: props.label,
+      data: usageLengthGroup,
+      fill: false,
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgba(255, 99, 132, 0.2)',
+    }]
+  }
+
+  return (
+    <div className={classes.chart}>
+      <div className={classes.center}>
+        <h2 className={classes.chartTitle}>{props.title}</h2>
+        <p className={classes.chartDates}>All time</p>
       </div>
       {/* @ts-ignore  */}
       <Line data={data} options={COMMON_LINE_CHART_OPTIONS}/>
