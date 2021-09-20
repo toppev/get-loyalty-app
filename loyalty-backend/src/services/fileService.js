1240977839da1a10b4bfe519f8a937f93405fc5c
toppev <44984598+toppev@users.noreply.github.com>
2021-09-20 17:42:47 +0300

    Simple limit for max uploads

--- loyalty-backend/src/services/fileService.js
+++ loyalty-backend/src/services/fileService.js
@@ -1,7 +1,15 @@
 const FileUpload = require('../models/fileUpload')
 const mime = require('mime-types')
+const StatusError = require("../util/statusError")
+const logger = require("../util/logger")
 
 async function getUpload(name, { visibility } = {}) {
+  const uploadsCountLimit = 100
+  if (await FileUpload.countDocuments({}) > uploadsCountLimit) {
+    const err = "Max uploads reached. Contact support."
+    logger.severe(err, { uploadsCountLimit })
+    throw new StatusError(401, err)
+  }
   const res = await FileUpload.findById(name)
   if (res.visibility === (visibility || 'public')) {
     return res
