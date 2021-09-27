262717d82ab61c6cd7e04816f7e4b78d9e75f24a
toppev <44984598+toppev@users.noreply.github.com>
2021-09-27 11:47:59 +0300

    Use TZ (timezone) in logs etc

--- loyalty-backend/src/util/logger.js
+++ loyalty-backend/src/util/logger.js
@@ -1,11 +1,11 @@
 const dateStr = () => {
   const date = new Date()
-  return date.getUTCFullYear() + "/" +
-    ("0" + (date.getUTCMonth() + 1)).slice(-2) + "/" +
-    ("0" + date.getUTCDate()).slice(-2) + " " +
-    ("0" + date.getUTCHours()).slice(-2) + ":" +
-    ("0" + date.getUTCMinutes()).slice(-2) + ":" +
-    ("0" + date.getUTCSeconds()).slice(-2)
+  return date.getFullYear() + "/" +
+    ("0" + (date.getMonth() + 1)).slice(-2) + "/" +
+    ("0" + date.getDate()).slice(-2) + " " +
+    ("0" + date.getHours()).slice(-2) + ":" +
+    ("0" + date.getMinutes()).slice(-2) + ":" +
+    ("0" + date.getSeconds()).slice(-2)
 }
 
 
