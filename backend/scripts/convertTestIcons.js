6606f8772cb851035ed9e835c8585096789d0825
toppev <44984598+toppev@users.noreply.github.com>
2021-12-15 16:01:10 +0200

    imp: update bunch of stuff + npm audit fix

--- backend/scripts/convertTestIcons.js
+++ backend/scripts/convertTestIcons.js
@@ -4,18 +4,37 @@
  * Check the new icons manually!
  */
 
-const iconService = require('../src/services/iconService')
+const sharp = require("sharp")
+const toIco = require("to-ico")
+const fs = require("fs")
+
+async function resizeToPNG(input, sizes, outputDir, name) {
+  const tasks = sizes.map(size => sharp(input).resize(size, size).toFile(`${outputDir}/${name}-${size}.png`))
+  return Promise.all(tasks)
+}
+
+async function generateFavicon(source, output) {
+  const image = await fs.promises.readFile(source);
+
+  const result = await toIco([image], {
+    sizes: [16, 24, 32, 48, 64],
+    resize: true
+  })
+  await fs.promises.writeFile(output, result)
+}
 
 
 const favicon = 'testresources/converted-favicon.ico'
+
 console.log(`Generating ${favicon}`)
-iconService.generateFavicon('./testresources/icon-192x192.png', favicon).then(() => {
+generateFavicon('./testresources/icon-192x192.png', favicon).then(() => {
   console.log(`${favicon} generated`)
 })
 
 
 const file512 = 'converted-icon'
+
 console.log(`Generating ${file512}`)
-iconService.resizeToPNG('./testresources/icon-192x192.png', [512], 'testresources', file512).then(() => {
+resizeToPNG('./testresources/icon-192x192.png', [512], 'testresources', file512).then(() => {
   console.log(`${file512} generated`)
 })
