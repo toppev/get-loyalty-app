43bd24aaa04c400162e960fed25aaea59d94a4b7
Topias <44984598+toppev@users.noreply.github.com>
2022-02-08 11:24:13 +0200

    feat: slightly better search

--- backend/src/services/customerService.js
+++ backend/src/services/customerService.js
@@ -179,17 +179,27 @@ async function rewardAllCustomers(reward) {
 
 /**
  * List customers of the business
- * @param limit maximum number of customers to return, defaults to 100 first if "search" is not specified,
- * otherwise 500. 0 for unlimited
- * @param search the string to search, if no limit is given, only the first 500 will be searched
+ * @param limit maximum number of customers to return
+ * @param search the string to search, if not specified first 100 are returned
  */
 async function searchCustomers(limit, search = "") {
-  // FIXME: this could be a lot better, just a temp solution
-  const trueLimit = limit === undefined ? (search ? 0 : 100) : limit
-  let users = await _listCustomers().limit(Math.max(0, trueLimit))
-  if (search?.trim()?.length) {
-    users = users.filter(u => JSON.stringify(u).toLowerCase().includes(search))
+  let users
+  if (search?.trim().length) {
+    const regex = new RegExp(search.split(" ").join("|"), "gi")
+    users = await User
+      .find({
+        $or: [
+          { "email": regex },
+          { "role": regex },
+          { "customerData.rewards.name": regex },
+        ]
+      })
+      .populate(userPopulateSchema)
+  } else {
+    users = await User.find()
+      .populate(userPopulateSchema)
   }
+  console.log({ users, limit, search })
   return Promise.all(users.map(u => getCustomerInfo(u)))
 }
 
