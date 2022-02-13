1ff39236c9e7b861365560ada67b20ff0d8375b9
Topias <44984598+toppev@users.noreply.github.com>
2022-02-13 16:11:18 +0200

    service worker stuff

--- panel/src/index.tsx
+++ panel/src/index.tsx
@@ -6,4 +6,12 @@ import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(<App/>, document.getElementById('root'))
 
-serviceWorker.register()
+serviceWorker.register({
+  onUpdate: async registration => {
+    if (registration && registration.waiting) {
+      await registration.unregister()
+      registration.waiting.postMessage({ type: 'SKIP_WAITING' })
+      window.location.reload()
+    }
+  },
+})
